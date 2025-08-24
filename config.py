aafc40e3797c30a3f74ff232c68527640070e52a
Bradjones77 <bradleyjones77@hotmail.com>
2025-08-24 13:18:30 +1200

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,14 @@
 TELEGRAM_TOKEN = "8249914971:AAHZsWf0mzExoMDe2VDLkP6xarmk1L9Vtjg"
 
 # Main coins
-COINS = ['BTC','ETH','USDT','BNB','XRP','ADA','DOGE','MATIC','SOL','DOT']
+COINS = [
+    'BTC','ETH','USDT','BNB','XRP','ADA','DOGE','MATIC','SOL','DOT'
+]
 
 # Meme coins
-MEME_COINS = ['DOGE','SHIB','APE','PEPE','MANA','CHZ','SAND','LUNA','LUNA2','XVS','CAKE']
+MEME_COINS = [
+    'DOGE','SHIB','APE','PEPE','MANA','CHZ','SAND','LUNA','LUNA2','XVS','CAKE'
+]
 
 # CoinMarketCap API key
 CMC_API_KEY = "2cc7c356-79e3-472d-853f-3db2675df271"
