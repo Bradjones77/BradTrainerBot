feb63603ac994e7e7410b78e5b000c4493692a4f
Bradjones77 <bradleyjones77@hotmail.com>
2025-08-24 13:15:33 +1200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
+# config.py
+
 TELEGRAM_TOKEN = "8249914971:AAHZsWf0mzExoMDe2VDLkP6xarmk1L9Vtjg"
 
-CMC_API_KEY = "YOUR_CMC_API_KEY"
+# Main coins
+COINS = ['BTC','ETH','USDT','BNB','XRP','ADA','DOGE','MATIC','SOL','DOT']
 
-COINS = ['BTC', 'ETH', 'USDT', 'BNB', 'XRP', 'ADA', 'DOGE', 'MATIC', 'SOL', 'DOT', 'SHIB', 'LTC', 'TRX', 'AVAX', 'UNI', 'CRO', 'NEAR', 'FTM', 'ATOM', 'ALGO', 'LINK', 'XLM', 'BCH', 'VET', 'ICP', 'FIL', 'EGLD', 'APE', 'EOS', 'THETA', 'HBAR', 'SAND', 'GRT', 'CHZ', 'KSM', 'STX', 'QNT', 'CFX', 'ZIL', 'ENJ', 'BAT', 'DCR', 'NEO', '1INCH', 'FLOW', 'LRC', 'ZRX', 'RUNE', 'CELO', 'AR', 'KAVA', 'MANA', 'UMA', 'REV', 'KNC', 'HNT', 'OKB', 'CRV', 'MINA', 'AUDIO', 'OCEAN', 'LPT', 'ANKR', 'GLM', 'CVX', 'BAL', 'SRM', 'IOST', 'XTZ', 'IOTA', 'XEM', 'QTUM', 'FTT', 'WAXP', 'MKR', 'DGB', 'HIVE', 'OGN', 'STORJ', 'LUNA', 'RSR', 'AMP', 'PEPE', 'WBTC', 'LDO', 'ARB', 'DYDX', 'OP', 'APT', 'SUI', 'TON', 'XMR', 'VET']
+# Meme coins
+MEME_COINS = ['DOGE','SHIB','APE','PEPE','MANA','CHZ','SAND','LUNA','LUNA2','XVS','CAKE']
+
+# CoinMarketCap API key
+CMC_API_KEY = "2cc7c356-79e3-472d-853f-3db2675df271"
